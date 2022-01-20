7faad27fa8a097d6417b13e0dc30f08cef4707cb
Tom H Anderson <tom.h.anderson@gmail.com>
2022-01-20 02:08:52 -0700

    Added empty ServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ApiSkeletons\Laravel\ApiProblem;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+    }
+}
